a16050aa88461a99656df40583ff6814a2682e9a
tumst <tumoioio@gmail.com>
2017-03-14 08:47:50 +0700

    init 14/03/2017

---
+++ application/controllers/test/c_stp.php
@@ -0,0 +1,32 @@
+<?php
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of c_stp
+ *
+ * @author Administrator
+ */
+class c_stp extends CI_Controller {
+    function __construct() {
+         parent::__construct();
+         
+    }
+    
+    function test_stp(){
+        echo 'test stp';
+        
+        
+    }
+   
+}
+
+
+
+/* End of file api_inventory_packaging */
+/* Location: ./application/controllers/api/api_inventory_packaging.php */
