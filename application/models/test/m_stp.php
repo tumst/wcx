184564ac8e15ebe81f22d3764a2738c9a9fbc488
tumst <tumoioio@gmail.com>
2017-03-14 09:05:14 +0700

    init 14/03/2017

---
+++ application/models/test/m_stp.php
@@ -0,0 +1,31 @@
+<?php
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of m_stp
+ *
+ * @author Administrator
+ */
+class m_stp extends CI_Model {
+    //put your code here
+    public function __construct() {
+        $this->load->database();
+        
+    }
+    
+    function test_stp(){
+        $this->load;
+            
+    }
+    
+}
+
+
+/* End of file m_stp.php */
+/* Location: ./application/models/test/m_stp.php */
